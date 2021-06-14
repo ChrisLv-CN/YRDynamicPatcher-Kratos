1abce7021617ebbe6524491134a9d4dad60df4f2
ChrisLv_CN <12737@live.com>
2021-06-15 03:35:10 +0800

    add

---
+++ DynamicPatcher/Scripts/ParaScript.cs
@@ -0,0 +1,38 @@
+
+using System.Net;
+using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using DynamicPatcher;
+using PatcherYRpp;
+using Extension.Ext;
+using Extension.Script;
+using Extension.Decorators;
+using Extension.Utilities;
+using System.Threading.Tasks;
+
+namespace Scripts
+{
+    [Serializable]
+    public class ParaScript : TechnoScriptable
+    {
+        private bool flag = false;
+
+        public ParaScript(TechnoExt owner) : base(owner) { }
+
+        public override void OnUpdate()
+        {
+            
+            Pointer<TechnoClass> pTechno = Owner.OwnerObject;
+            if (!flag && pTechno.Convert<ObjectClass>().Ref.GetCurrentMission() == Mission.Hunt)
+            {
+                flag = true;
+                pTechno.Convert<MissionClass>().Ref.QueueMission(Mission.Area_Guard, false);
+            }
+        }
+
+    }
+
+}
+
