f81ff2bd6233eab40cfc6e1f54de4e144b0bdae1
ChrisLv <12737@live.com>
2022-07-21 00:57:41 +0700

    update Kraots-version show

--- DynamicPatcher/ExtensionHooks/GScreenExt.cs
+++ DynamicPatcher/ExtensionHooks/GScreenExt.cs
@@ -11,13 +11,23 @@ namespace ExtensionHooks
 {
     public class GScreenExtHook
     {
-        [Hook(HookType.AresHook, Address = 0x4F4583, Size = 6)]
+        // [Hook(HookType.AresHook, Address = 0x4F4497, Size = 6)] // GScreenClass_Render
+        [Hook(HookType.AresHook, Address = 0x4F4583, Size = 6)] // GScreenClass_Render
         public static unsafe UInt32 GScreenClass_Render(REGISTERS* R)
         {
-            Kratos.DrawActiveMessage();
+            // Logger.Log($"{Game.CurrentFrame} GScreenClass_Render call");
             PrintTextManager.PrintText();
             return 0;
         }
+
+        [Hook(HookType.AresHook, Address = 0x6A70AE, Size = 5)] // SidebarClass_Draw_It
+        public static unsafe UInt32 SidebarClass_Draw_It(REGISTERS* R)
+        {
+            // Logger.Log($"{Game.CurrentFrame} SidebarClass_Draw_It call"); // before GScreen
+            Kratos.DrawActiveMessage();
+            return 0;
+        }
+
     }
 }
 
