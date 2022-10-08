671603f7179ccdcc24dbd4e122507e8c5486dbaa
ChrisLv <12737@live.com>
2022-10-08 23:39:07 +0700

    调整版本号为7.3

--- DynamicPatcher/Projects/Extension/Properties/AssemblyInfo.cs
+++ DynamicPatcher/Projects/Extension/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("2.7.0.0")] // DP.Kratos.x.x
+[assembly: AssemblyFileVersion("2.7.3.0")] // DP.Kratos.x.x
